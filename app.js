bea3c0c8e5305b8612af8321ffcd0b4bca021e39
Mukhthar Azeez <mukhtharazeez28@gmail.com>
2022-11-10 06:48:40 +0530

    preloader fixed

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// const createError = require('http-errors');
+const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
@@ -60,9 +60,9 @@ app.use('/admin', adminRouter);
 app.use('/', userRouter);
 
 // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
 // error handler
 app.use(function(err, req, res, next) {
